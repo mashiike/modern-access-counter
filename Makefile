6940b65a29830645ecacfaae54939597bff4af9a
mashiike <ikeda-masashi@kayac.com>
2021-11-09 18:46:33 +0900

    api-gateway resource import and makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+TFSTATE_PATH:=terraform/.terraform/terraform.tfstate
+
 .PHONY: terraform/plan
 terraform/plan:
 	cd terraform && \
@@ -9,18 +11,29 @@ terraform/apply:
 	cd terraform && \
 	terraform apply
 
+.PHONY: terraform/apply-only-iam
+terraform/apply-only-iam:
+	cd terraform && \
+	terraform apply -target={aws_iam_role.function,aws_iam_role_policy.function,aws_s3_bucket.data}
+
 bootstrap:
 	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
 	go build -o bootstrap .
 
 .PHONY: deploy
 deploy: bootstrap
-	lambroll --tfstate terraform/.terraform/terraform.tfstate deploy
+	lambroll --tfstate $(TFSTATE_PATH) deploy
 
 .PHONY: deploy-dry-run
 deploy-dry-run: bootstrap
-	lambroll --tfstate terraform/.terraform/terraform.tfstate deploy --dry-run
+	lambroll --tfstate $(TFSTATE_PATH) deploy --dry-run
 
 .PHONY: reset-counter
 reset-counter:
 	aws s3 rm s3://${S3_BUCKET}/${S3_OBJECT_PATH}
+
+total_access:=500
+parallelism:=20
+.PHONY: reset-counter
+load-test:
+	ab -n ${total_access} -c ${parallelism} $(shell tfstate-lookup -state ${TFSTATE_PATH} aws_apigatewayv2_api.main.api_endpoint)/
--- terraform/resource.tf
+++ terraform/resource.tf
@@ -70,3 +70,49 @@ resource "aws_iam_role_policy" "function" {
     }
   )
 }
+
+data "aws_lambda_function" "main" {
+  function_name = "modern-access-counter"
+}
+
+resource "aws_apigatewayv2_api" "main" {
+  name          = "modern-access-counter"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.main.id
+  name        = "$default"
+  auto_deploy = true
+}
+resource "aws_apigatewayv2_integration" "main" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  integration_uri        = data.aws_lambda_function.main.arn
+  integration_type       = "AWS_PROXY"
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.main.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_route" "root" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  route_key = "ANY /"
+  target    = "integrations/${aws_apigatewayv2_integration.main.id}"
+}
+
+resource "aws_apigatewayv2_route" "gif" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  route_key = "ANY /counter.gif"
+  target    = "integrations/${aws_apigatewayv2_integration.main.id}"
+}
